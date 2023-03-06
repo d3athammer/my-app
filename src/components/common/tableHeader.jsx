861cf53b09c73d9427539b44ceebd16d438f0631
ting wei yap <yap.tingwei@gmail.com>
2023-03-06 17:44:55 +0800

    create icon for sorting header

--- src/components/common/tableHeader.jsx
+++ src/components/common/tableHeader.jsx
@@ -17,12 +17,29 @@ class TableHeader extends Component {
   this.props.onSort(sortColumn);
   };
 
+  renderSortIcon = column => {
+    const { sortColumn } = this.props
+    //1. icon will show both sides when no filter are selected
+    if (column.path !== sortColumn.path) return <i className="fa-solid fa-sort"></i>;
+    //2. icon will show sort up when in ascending order
+    if (sortColumn.order === 'asc') return <i class="fa-solid fa-sort-up"></i>;
+    //3. icon will show sort down when in descending order
+    return <i class="fa-solid fa-sort-down"></i>;
+  }
+
   render() {
     return (
       <thead>
         <tr>
           {this.props.columns.map(column => (
-            <th key={column.path || column.key} onClick={() => this.raiseSort(column.path)} >{column.label}</th>
+            <th
+              style={{cursor: "pointer"}}
+              key={column.path || column.key}
+              onClick={() => this.raiseSort(column.path)}
+            >
+              {column.label} {this.renderSortIcon(column)}
+            </th>
+
           ))}
         </tr>
       </thead>
